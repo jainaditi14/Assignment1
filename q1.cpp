b7e080a240ae58bef0405d995dcc4152f389685c
jainaditi14 <22103310@mail.jiit.ac.in>
2024-07-27 16:35:10 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
 
-int n;
-cout<<Enter the number of integers;
-cin>>n;
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
-vector<int> v(n);
+    double sum = accumulate(arr.begin(), arr.end(), 0);
+    double average = sum / n;
 
-while(n){
-    cin>>v[i];
-}
+    cout << "The average is: " << average << endl;
 
+    return 0;
 }
