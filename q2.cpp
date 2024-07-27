f25e0bf2e47376059c17c5cd0bd55550b6a57f73
jainaditi14 <22103310@mail.jiit.ac.in>
2024-07-27 16:35:54 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str); 
+
+    reverse(str.begin(), str.end());
+
+    cout << "The reversed string is: " << str << endl;
+
+    return 0;
+}
 
